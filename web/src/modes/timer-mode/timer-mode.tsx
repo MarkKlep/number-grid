17aa31fc372e0b97da37db85ff67c197588db1c0
mark <cheapshoterr@gmail.com>
2024-06-25 03:55:34 +0300

    timer mode game done

--- web/src/modes/timer-mode/timer-mode.tsx
+++ web/src/modes/timer-mode/timer-mode.tsx
@@ -1,34 +1,45 @@
-import React, { useState, useEffect, FC } from 'react'
+import React, { useState, useEffect, useRef, FC } from 'react'
 import { matrixNumbersGenerator } from '../../utilities/matrix-processing';
 import { GridRenderer } from './grid-renderer';
 import gridNumberClickSound from '../../assets/grid-number-click.mp3';
 import { GridCell } from '../../types/timer-mode';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import '../../styles/timer-mode-panel.scss';
 
 type TimerModeProps = {
     gridSize: number,
 }
 
+const generateGrid = (gridSize: number): GridCell[][] => {
+    const numsMatrix = matrixNumbersGenerator(gridSize);
+
+    const grid = numsMatrix.map((row, rowIndex) => {
+        return row.map((cell, cellIndex) => {
+            return {
+                value: cell,
+                rowIndex,
+                cellIndex,
+                done: false,
+                failed: false,
+            }
+        })
+    })
+
+    return grid;
+}
+
 export const TimerMode: FC<TimerModeProps> = (props) => {
     const { gridSize } = props;
-    const [gridCells, setGridCells] = useState<GridCell[][]>([]);
+    const [gridCells, setGridCells] = useState<GridCell[][]>(generateGrid(gridSize));
     const [timer, setTimer] = useState<number>(0);
 
-    const generateGrid = () => {
-        const numsMatrix = matrixNumbersGenerator(gridSize);
+    const intervalID = useRef<any>(null);
 
-        setGridCells(
-            numsMatrix.map((row, rowIndex) => {
-                return row.map((cell, cellIndex) => {
-                    return {
-                        value: cell,
-                        rowIndex,
-                        cellIndex,
-                        done: false,
-                        failed: false,
-                    }
-                })
-            })
-        );
+    const gameIsOver = () => {
+        return gridCells.flat().every(cell => cell.done || cell.failed);
     }
 
     const ascOrderWrongClick = (rowIndex: number, cellIndex: number): boolean => {
@@ -78,22 +89,34 @@ export const TimerMode: FC<TimerModeProps> = (props) => {
     }
 
     useEffect(() => {
-        generateGrid();
-
-        const intervalID = setInterval(() => {
+        intervalID.current = setInterval(() => {
             setTimer(timer => timer + 1);
         }, 1000);
 
         return () => {
-            clearInterval(intervalID);
+            clearInterval(intervalID.current);
         }
     }, []);
 
+    useEffect(() => {
+        if(gameIsOver()) {
+            clearInterval(intervalID.current);
+        }
+
+    }, [gridCells]);
+
     return (
         <div>
-            <div className="timer">
-                {timer}
-            </div>
+            <Container className='timer-mode-panel'>
+                <Row>
+                    <Col>
+                        <Badge bg="warning" text="dark" className="timer-badge">
+                            Timer: {timer} sec.
+                        </Badge>
+                    </Col>
+                </Row>
+            </Container>
+
             {
                 gridCells.length > 0 && (
                     <GridRenderer 
