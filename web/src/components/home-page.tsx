b66deae879845aa2f4b04991c8391e044a5ece8e
Mark Klepanchuk <cheapshoterr@gmail.com>
2024-07-10 02:04:46 +0300

    styled navbar

--- web/src/components/home-page.tsx
+++ web/src/components/home-page.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Stack, Box } from '@mui/material';
 import { Link, Outlet } from 'react-router-dom';
+import '../styles/home-page.scss';
 
 export const HomePage = () => {
   return (
     <div>
+      <Box className='navbar'>
+        <Stack className='links-container' direction='row' justifyContent='space-around' alignItems='center'>
+          <Link to='/'>Home</Link>
+          <Link to='reg-form'>Register</Link>
+        </Stack>
+      </Box>
 
-        <Link to='/'>Home</Link>
-        <Link to='reg-form'>Register</Link>
-
-        <Outlet />
+      <Outlet />
 
     </div>
   )
