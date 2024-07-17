de525817e0d481d21b69c9558be7380bef26bb00
Mark Klepanchuk <cheapshoterr@gmail.com>
2024-07-17 19:51:47 +0300

    space test

--- web/src/utilities/validationSchema.ts
+++ web/src/utilities/validationSchema.ts
@@ -1,10 +1,17 @@
 import * as yup from 'yup';
 
+const isNoSpaces = (value: string) => {
+    return !/\s/.test(value);
+}
+
 export const regSchema = yup.object({
-    name: yup.string().required().min(4),
-    email: yup.string().required().email("Invalid email"),
+    name: yup.string().required().min(4)
+    .test('has-no-space', 'Name must not contain spaces', value => isNoSpaces(value)),
+    email: yup.string().required().email("Invalid email")
+    .test('has-no-space', 'Email must not contain spaces', value => isNoSpaces(value)),
     password: yup.string().required()
     .min(8, "Password must be at least 8 characters").max(20, "Password must be at most 20 characters")
+    .test('has-no-space', 'Password must not contain spaces', value => isNoSpaces(value))
     .test('has-letter', 'Password must contain at least one letter', value => /[a-z]/.test(value))
     .test('has-upper-case', 'Password must contain at least one upper case letter', value => /[A-Z]/.test(value))
     .test('has-number', 'Password must contain at least one number', value => /\d/.test(value))
