fa92017ee8f21c6d3681024f01dbf173c10182df
mark <cheapshoterr@gmail.com>
2024-07-25 03:24:07 +0300

    creation linter + formater

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,13 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.post('/reg-user', (req, res) => {
+app.post("/reg-user", (req, res) => {
   console.log(req.body);
-  res.status(201).send('User registered successfully');
+  res.status(201).send("User registered successfully");
 });
 
 app.listen(PORT, () => {
